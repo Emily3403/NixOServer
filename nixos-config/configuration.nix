3f6edb985e403fe9c4ba5ed38a0aab482704d023
Emily3403 <seebeckemily3403@gmail.com>
2023-04-11 15:27:50 +0200

    Modified: nixos-config/configuration.nix

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -5,134 +5,82 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+    imports =
+        [ # Include the results of the hardware scan.
+            ./hardware-configuration.nix
+        ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "22.11"; # Did you read the comment?
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+    nix.settings.experimental-features = lib.mkDefault [ "nix-command"];
 
+    # Copy the NixOS configuration file and link it from the resulting system
+    # (/run/current-system/configuration.nix). This is useful in case you
+    # accidentally delete configuration.nix.
+    system.copySystemConfiguration = true;
 
-   networking.hostName = "ruwushOnNixOS"; # Define your hostname.
-   networking.networkmanager.enable = true;  # Enable Networking
+    # Use the GRUB 2 boot loader.
+    boot.loader.grub.enable = true;
+    boot.loader.grub.version = 2;
+    # boot.loader.grub.efiSupport = true;
+    # boot.loader.grub.efiInstallAsRemovable = true;
+    # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+    # Define on which hard drive you want to install Grub.
+    # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  # Set your time zone.
-   time.timeZone = "Europe/Berlin";
+    networking.hostName = "ruwushOnNixOS"; # Define your hostname.
+    networking.networkmanager.enable = true;  # Enable Networking
+    # TODO: Firewall?
 
-  # Set the default user account.
-  users.users = {
-    root = {
-      initialHashedPassword = "rootHash_placeholder";
-      openssh.authorizedKeys.keys = [ "sshKey_placeholder" ];
+    # Configure Systemd services
+    services.openssh = {
+        enable = lib.mkDefault true;
+        passwordAuthentication = lib.mkDefault false;
     };
 
-    emily = {
-      initialHashedPassword = "!";
-      isNormalUser = true;
-      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for your user.
-      uid = 1000;
+    # Set your time zone.
+    time.timeZone = "Europe/Berlin";
+
+    # Set the default user account.
+    users.users = {
+        root = {
+            initialHashedPassword = "rootHash_placeholder";
+            openssh.authorizedKeys.keys = [ "sshKey_placeholder" ];
+        };
+
+        emily = {
+            initialHashedPassword = "!";
+            isNormalUser = true;
+            extraGroups = [ "wheel" ]; # Enable ‘sudo’ for your user.
+            uid = 1000;
+        };
     };
-  };
 
-  users.users.
+    programs.neovim = {
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
+    };
 
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-  };
+    programs.git.enable = true;
 
+    # Select console keymap.
+     console = {
+       font = "Lat2-Terminus16";
+       keyMap = "de-latin1";
+       useXkbConfig = true; # use xkbOptions in tty.
+     };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     thunderbird
-  #   ];
-  # };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
+    # Packages to be installed
+     environment.systemPackages = with pkgs; [
+       neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+       wget
+       git
+     ];
 }
-
