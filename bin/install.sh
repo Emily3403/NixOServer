193fe5e7efe0ebb26dac8f47773a7b8077f69620
Emily3403 <seebeckemily3403@gmail.com>
2023-04-11 02:05:51 +0200

    Modified: bin/install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -27,7 +27,7 @@ set -e
 
 # Install dependencies
 apt update
-apt install -y git vim openssh-server gdisk
+apt install -y git vim openssh-server gdisk dosfstools
 
 # Backup the existing sources.list file
 cp /etc/apt/sources.list /etc/apt/sources.list.backup."$(date --iso)"
