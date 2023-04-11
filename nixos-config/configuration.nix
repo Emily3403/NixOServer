45ce668d936cb38c5a549fc71065dd842d9289f2
Emily3403 <seebeckemily3403@gmail.com>
2023-04-11 15:32:15 +0200

    Modified: nixos-config/configuration.nix

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -85,6 +85,7 @@
     # Packages to be installed
      environment.systemPackages = with pkgs; [
        neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+       vim
        wget
        git
      ];
