33521c565bb4af0e92133d2c44f82d538c0a63df
Emily3403 <seebeckemily3403@gmail.com>
2023-04-11 15:51:42 +0200

    Modified: bin/clean.sh

--- bin/clean.sh
+++ bin/clean.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source "$SCRIPT_DIR/config.sh"
 source "$SCRIPT_DIR"/../InstallScripts/00diskSetup.sh
 
-sudo umount "${DRIVES[@]}" || true
+#sudo umount "${DRIVES[@]}" || true
 
 sudo zpool destroy bpool || true
 sudo zpool destroy rpool || true
