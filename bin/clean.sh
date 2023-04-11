15ddc2b5d99dfdc71aede377c59d58dfb9c862a9
Emily3403 <seebeckemily3403@gmail.com>
2023-04-11 14:09:23 +0200

    Modified: bin/clean.sh

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO
-
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source "$SCRIPT_DIR/config.sh"
 source "$SCRIPT_DIR"/../InstallScripts/00diskSetup.sh
@@ -11,3 +9,5 @@ sudo umount "${DRIVES[@]}" || true
 sudo zpool destroy bpool || true
 sudo zpool destroy rpool || true
 sudo wipefs --all "${DRIVES[@]}"
+
+sync && udevadm settle
