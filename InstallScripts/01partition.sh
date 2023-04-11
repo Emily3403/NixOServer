b8202dc04f1143b877d566e6afc41c70190426ec
Emily3403 <seebeckemily3403@gmail.com>
2023-04-11 16:43:23 +0200

    Modified: InstallScripts/01partition.sh

--- InstallScripts/01partition.sh
+++ InstallScripts/01partition.sh
@@ -17,7 +17,9 @@ for disk in "${DRIVES[@]}"; do
 
     sgdisk --zap-all $disk
 
-    sgdisk -n1:0:+4G -t1:BE00 $disk
+    sgdisk -n1:1M:+1G -t1:EF00 $disk
+
+    sgdisk -n2:0:+4G -t2:BE00 $disk
 
     sgdisk -n4:0:+${EFFECTIVE_SWAP_PER_DRIVE}G -t4:8200 $disk
 
