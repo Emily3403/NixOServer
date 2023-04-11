f3b8bc6a988c7af99121309a482d47830a41f4c5
Emily3403 <seebeckemily3403@gmail.com>
2023-04-11 18:03:30 +0200

    Modified: InstallScripts/05setupNixOS.sh

--- InstallScripts/05setupNixOS.sh
+++ InstallScripts/05setupNixOS.sh
@@ -8,6 +8,10 @@ return
 curl -L https://nixos.org/nix/install | sh
 source "$HOME/.nix-profile/etc/profile.d/nix.sh"
 
+# Create the necessary groups
+sudo groupadd -g 30000 nixbld
+sudo useradd -u 30000 -g nixbld -G nixbld nixbld
+
 # Fetch the latest NixOS channel:
 nix-channel --add https://nixos.org/channels/nixos-22.11 nixpkgs
 nix-channel --update
