c9ebac90a0dbc8cadb98f5255e0e480a729e3afc
Emily3403 <seebeckemily3403@gmail.com>
2023-04-11 16:42:11 +0200

    Modified: InstallScripts/04setupConfig.sh

--- InstallScripts/04setupConfig.sh
+++ InstallScripts/04setupConfig.sh
@@ -8,7 +8,7 @@ mkdir -p /mnt/etc/nixos/
 cp -r "$SCRIPT_DIR"/../NixDotfiles/* /mnt/etc/nixos
 
 
-for i in $DRIVES; do
+for i in "${DRIVES[@]}"; do
   sed -i \
   "s|/dev/disk/by-id/|${i%/*}/|" \
   /mnt/etc/nixos/hosts/exampleHost/default.nix
@@ -16,7 +16,7 @@ for i in $DRIVES; do
 done
 
 diskNames=""
-for i in $DRIVES; do
+for i in "${DRIVES[@]}"; do
   diskNames="$diskNames \"${i##*/}\""
 done
 
@@ -29,7 +29,7 @@ sed -i "s|\"abcd1234\"|\"$(head -c4 /dev/urandom | od -A none -t x4| sed 's| ||g
 sed -i "s|\"x86_64-linux\"|\"$(uname -m)-linux\"|g" \
   /mnt/etc/nixos/flake.nix
 
-for i in $DRIVES; do
+for i in "${DRIVES[@]}"; do
   sed -i \
   "s|PLACEHOLDER_FOR_DEV_NODE_PATH|\"${i%/*}/\"|" \
   /mnt/etc/nixos/configuration.nix
@@ -38,7 +38,7 @@ done
 
 # Customize configuration to your hardware
 diskNames=""
-for i in $DRIVES; do
+for i in "${DRIVES[@]}"; do
   diskNames="$diskNames \"${i##*/}\""
 done
 tee -a /mnt/etc/nixos/machine.nix <<EOF
