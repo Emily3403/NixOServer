74abe8e64bb6f8df0e1c2c3f5e01734258d9a2f6
Emily3403 <seebeckemily3403@gmail.com>
2023-04-11 18:03:40 +0200

    Added: NixDotfiles/substituted/configuration.nix NixDotfiles/substituted/defaultConfiguration.nix NixDotfiles/substituted/flake.lock NixDotfiles/substituted/flake.nix NixDotfiles/substituted/hosts/exampleHost/changeHostName.txt NixDotfiles/substituted/hosts/exampleHost/default.nix NixDotfiles/substituted/hosts/exampleHost/sshUnlock.txt NixDotfiles/substituted/machine.nix NixDotfiles/substituted/modules/boot/default.nix NixDotfiles/substituted/modules/default.nix NixDotfiles/substituted/modules/fileSystems/default.nix NixDotfiles/substituted/modules/networking/default.nix NixDotfiles/substituted/README.org

---
+++ NixDotfiles/substituted/hosts/exampleHost/default.nix
@@ -0,0 +1,41 @@
+# #
+##
+##  per-host configuration for exampleHost
+##
+##
+
+{ system, pkgs, ... }: {
+  inherit pkgs system;
+  zfs-root = {
+    boot = {
+      devNodes = "/dev/disk/by-id/";
+      bootDevices = [  "ata-QEMU_HARDDISK_QM00003" "ata-QEMU_HARDDISK_QM00005" "ata-QEMU_HARDDISK_QM00007" "ata-QEMU_HARDDISK_QM00009" ];
+      immutable = false;
+      availableKernelModules = [
+        # for booting virtual machine
+        # with virtio disk controller
+        "virtio_pci"
+        "virtio_blk"
+        # for sata drive
+        "ahci"
+        # for nvme drive
+        "nvme"
+        # for external usb drive
+        "uas"
+      ];
+      removableEfi = true;
+      kernelParams = [ ];
+      sshUnlock = {
+        # read sshUnlock.txt file.
+        enable = false;
+        authorizedKeys = [ ];
+      };
+    };
+    networking = {
+      # read changeHostName.txt file.
+      hostName = "exampleHost";
+      timeZone = "Europe/Berlin";
+      hostId = "89bdf233";
+    };
+  };
+}
