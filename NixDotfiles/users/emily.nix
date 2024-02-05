9b919d992e8a0affe71c90649bd8730f53fcb761
Emily3403 <seebeckemily3403@gmail.com>
2024-02-05 10:19:00 +0100

    Test password authentication

--- NixDotfiles/users/emily.nix
+++ NixDotfiles/users/emily.nix
@@ -9,6 +9,7 @@
     uid = 1042;
     extraGroups = [ "wheel" ];
 
+    initialHashedPassword = "!";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHAzQFMYrSvjGtzcOUbR1YHawaPMCBDnO4yRKsV7WHkg emily"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINA0V/ByFxlMU8nBJ+R2RGxr0uZAapovARLPbHYmNE2V emily"
