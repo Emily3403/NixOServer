4cbd62dcc7f5f87d45af87405d11d39dc34ee11f
Emily3403 <seebeckemily3403@gmail.com>
2024-04-05 13:17:16 +0200

    Update Syncthing ForwardPorts, Add OwO

--- NixDotfiles/services/Syncthing.nix
+++ NixDotfiles/services/Syncthing.nix
@@ -20,6 +20,8 @@ let DATA_DIR = "/data/Syncthing"; in
           "/var/lib/syncthing/" = { hostPath = "${DATA_DIR}/syncthing"; isReadOnly = false; };
         };
 
+        additionalContainerConfig.forwardPorts = [ { hostPort = 22000; } { hostPort = 22000; protocol = "udp"; } { hostPort = 21027; protocol = "udp"; } ];
+
         cfg = {
           services.syncthing = {
             enable = true;
@@ -38,6 +40,7 @@ let DATA_DIR = "/data/Syncthing"; in
 
                 nyaa = { id = "6OGY4LN-KQ3XE33-X5QIWVN-IUZ6F5E-7DNZHQ6-7DVBR6G-FIVWZPC-GMKYXQN"; } // defconfig;
                 UwU = { id = "3P2KUWI-C7GCARO-LAHCSIB-M3O7LE7-X4RFYQ6-7HNFJ7I-Y72NUOV-3HNYAAA"; } // defconfig;
+                OwO = { id = "OZMTLE4-QNCFAYO-SGYCAHG-PPFQNU5-VJ7KBEI-OE3OOBT-3FQ3UGQ-HB3F5A6"; } // defconfig;
                 MightyMarshmellow = { id = "JB4YIH6-TXCJIWD-LRWIQYK-D6EUPAD-ER2BCUA-NEGOXO5-R4AUKI6-ME6QOAQ"; } // defconfig;
                 Pixel = { id = "XLVNMQQ-Q7UNI5E-IR3PCGW-OYHIM5Z-W2ZAWLQ-SIT7ZUG-IDYNMPO-ODTDYQ6"; } // defconfig;
                 UwUonWindows = { id = "XSQSL6O-RBBBCWY-DO44ILU-AT6DCSR-F5QR7U4-E4ZQHBT-RVSLUWK-CWWUTQ7"; } // defconfig;
